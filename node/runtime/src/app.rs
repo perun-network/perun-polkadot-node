eb2ec14b4f43f3aaa7c3cdf696501b308841eacd
Matthias Geihs <matthias@perun.network>
2022-08-16 18:06:03 +0200

    WIP use scale decode

--- node/runtime/src/app.rs
+++ node/runtime/src/app.rs
@@ -1,4 +1,6 @@
-use sp_std::{vec::Vec, convert::TryFrom};
+use codec::{Encode, Decode};
+use frame_support::RuntimeDebug;
+use sp_std::convert::TryFrom;
 use pallet_perun::{pallet::Config, types::{ParamsOf, StateOf, BalanceOf, ParticipantIndex}};
 
 macro_rules! require {
@@ -10,12 +12,15 @@ macro_rules! require {
 }
 
 const NUM_PARTS: usize = 2;
-const ACTOR_IDX: usize = 0;
-const GRID_IDX: usize = 1;
-const GRID_IDX_END: usize = 10;
-const EMPTY: u8 = 0;
-const PLAYER1: u8 = 1;
-const PLAYER2: u8 = 2;
+const FIELD_EMPTY: u8 = 0;
+const FIELD_PLAYER1: u8 = 1;
+const FIELD_PLAYER2: u8 = 2;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq, RuntimeDebug)]
+struct TicTacToeAppData {
+	pub next_actor: u8,
+	pub grid: [u8; 9],
+} 
 
 pub fn valid_transition<T: Config>(
   params: &ParamsOf<T>,
@@ -25,36 +30,41 @@ pub fn valid_transition<T: Config>(
 ) -> bool {
   if params.participants.len() != NUM_PARTS {
     return false;
-  } else if to.data.len() != GRID_IDX_END {
-    return false;
   }
 
+  let from_data = TicTacToeAppData::decode(&mut from.data.as_slice()).unwrap();
+  let to_data = TicTacToeAppData::decode(&mut to.data.as_slice()).unwrap();
+
   // Check actor.
-  let actor = from.data[ACTOR_IDX];
-  if ACTOR_IDX != usize::try_from(signer).unwrap() {
+  let actor = from_data.next_actor;
+  let signer_u8 = u8::try_from(signer).unwrap();
+  let num_parts = u8::try_from(NUM_PARTS).unwrap();
+  if actor != signer_u8 {
     return false;
-  } else if (actor + 1) % u8::try_from(NUM_PARTS).unwrap() != to.data[ACTOR_IDX] {
+  } else if (actor + 1) % num_parts != to_data.next_actor {
     return false;
   }
 
   // Check action.
   let mut changed = false;
-  for i in GRID_IDX..GRID_IDX_END {
-    // Check value.
-    if !valid_value(to.data[i]) {
-      return false;
-    } else if from.data[i] != to.data[i] {
+  for (i, v) in to_data.grid.iter().enumerate() {
+    if !valid_value(*v) {
+      return false; // Invalid value.
+    }
+    
+    let from_v = from_data.grid[i];
+    if from_v != *v {
       if changed {
-        return false;
-      } else if from.data[i] != EMPTY {
-        return false;
+        return false; // Two moves in one.
+      } else if from_v != FIELD_EMPTY {
+        return false; // Field overwritten.
       }
       changed = true;
     }
   }
 
   // Check final.
-  let (is_final,has_winner, winner) = check_final(&to.data);
+  let (is_final,has_winner, winner) = check_final(&to_data);
   require!(to.finalized == is_final);
 
   // Check balances.
@@ -69,10 +79,10 @@ pub fn valid_transition<T: Config>(
 }
 
 fn valid_value(v: u8) -> bool {
-  return v == EMPTY || v == PLAYER1 || v == PLAYER2;
+  return v == FIELD_EMPTY || v == FIELD_PLAYER1 || v == FIELD_PLAYER2;
 }
 
-fn check_final(data: &Vec<u8>) -> (bool, bool, usize) {
+fn check_final(data: &TicTacToeAppData) -> (bool, bool, usize) {
   // 0 1 2
   // 3 4 5
   // 6 7 8
@@ -86,27 +96,27 @@ fn check_final(data: &Vec<u8>) -> (bool, bool, usize) {
   for v in winning.iter() {
       let (ok, v) = same_value(data, v);
       if ok {
-          if v == PLAYER1 {
+          if v == FIELD_PLAYER1 {
               return (true, true, 0);
-          } else if v == PLAYER2 {
+          } else if v == FIELD_PLAYER2 {
               return (true, true, 1);
           }
       }
   }
 
   // Check all set.
-  for f in data[GRID_IDX..GRID_IDX_END].iter() {
-      if f != &EMPTY {
+  for f in data.grid.iter() {
+      if f != &FIELD_EMPTY {
           return (false, false, 0);
       }
   }
   return (true, false, 0);
 }
 
-fn same_value(data: &Vec<u8>, v: &[usize; 3]) -> (bool, u8) {
-  let first = data[GRID_IDX + v[0]];
+fn same_value(data: &TicTacToeAppData, v: &[usize; 3]) -> (bool, u8) {
+  let first = data.grid[v[0]];
   for f in v[1..].iter() {
-      if data[GRID_IDX + f] != first {
+      if data.grid[*f] != first {
           return (false, 0);
       }
   }
